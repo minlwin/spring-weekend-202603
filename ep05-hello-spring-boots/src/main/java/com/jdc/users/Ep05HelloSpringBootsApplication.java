c6a8519a0ae31a58af6080d89ef9f621cd0b5c6c
minlwin <lwin.zawmin@gmail.com>
2026-03-14 18:43:50 +0630

    Share Hello Spring Boots

---
+++ ep05-hello-spring-boots/src/main/java/com/jdc/users/Ep05HelloSpringBootsApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ep05HelloSpringBootsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ep05HelloSpringBootsApplication.class, args);
+	}
+
+}
---
+++ ep05-hello-spring-boots/src/test/java/com/jdc/users/Ep05HelloSpringBootsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ep05HelloSpringBootsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
