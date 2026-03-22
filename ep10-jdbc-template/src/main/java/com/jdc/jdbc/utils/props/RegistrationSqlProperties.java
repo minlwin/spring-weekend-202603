4a021a154cb4d1ffe5134e77cb4dfc287dc214d4
minlwin <lwin.zawmin@gmail.com>
2026-03-22 10:46:44 +0630

    Update Configuration Properties

---
+++ ep10-jdbc-template/src/main/java/com/jdc/jdbc/utils/props/RegistrationSqlProperties.java
@@ -0,0 +1,17 @@
+package com.jdc.jdbc.utils.props;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "app.sql.registrations")
+public class RegistrationSqlProperties {
+
+	private String insert;
+	private String select;
+	private String findById;
+	private String delete;	
+}
