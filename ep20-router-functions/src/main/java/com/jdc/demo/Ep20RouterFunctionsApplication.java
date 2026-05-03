e770ee773288b0e07c7cec95b0c2f578efc4c524
minlwin <lwin.zawmin@gmail.com>
2026-05-03 14:30:13 +0630

    Controller Types

---
+++ ep20-router-functions/src/main/java/com/jdc/demo/Ep20RouterFunctionsApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ep20RouterFunctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ep20RouterFunctionsApplication.class, args);
+	}
+
+}
