f8fc69f23a0d8ab9fc8e433e72ab6ab80570f3fe
minlwin <lwin.zawmin@gmail.com>
2026-05-31 13:09:33 +0630

    Update UI for Classes

--- ep26-using-jpa/src/main/java/com/jdc/demo/controller/ClassesController.java
+++ ep26-using-jpa/src/main/java/com/jdc/demo/controller/ClassesController.java
@@ -1,22 +1,54 @@
 package com.jdc.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jdc.demo.domain.entity.Course.Level;
+import com.jdc.demo.domain.input.CourseSearch;
+import com.jdc.demo.service.CourseService;
 
 @Controller
 @RequestMapping("classes")
 public class ClassesController {
+	
+	@Autowired
+	private CourseService courseService;
 
 	@GetMapping
 	String search() {
 		return "classes/list";
 	}
 	
+	@GetMapping("{id}")
+	String findById(@PathVariable int id) {
+		return "classes/details";
+	}
+	
+	@GetMapping("edit")
+	String addNew() {
+		return "classes/edit";
+	}
+	
+	@GetMapping("edit/{id}")
+	String edit(@PathVariable int id) {
+		return "classes/edit";
+	}
+	
+	@PostMapping("edit")
+	String save() {
+		return "redirect:/classes/%s".formatted("1");
+	}
+
 	@ModelAttribute
 	void getTitle(ModelMap model) {
 		model.put("title", "Classes");
+		model.put("courses", courseService.search(new CourseSearch()));
+		model.put("levels", Level.values());
 	}
 }
