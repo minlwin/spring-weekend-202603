27641a8cb52e39eb75f3e4ab0b47583329b9d08c
minlwin <lwin.zawmin@gmail.com>
2026-06-07 16:02:01 +0630

    Commit Security

---
+++ ep30-gettting-start-security/src/main/java/com/jdc/demo/Ep30GetttingStartSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ep30GetttingStartSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ep30GetttingStartSecurityApplication.class, args);
+	}
+
+}
---
+++ ep31-user-details-service/src/test/java/com/jdc/demo/Ep31UserDetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ep31UserDetailsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
